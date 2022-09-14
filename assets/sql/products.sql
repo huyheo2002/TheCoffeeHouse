cd7b71271676c7185bc81db6d72c25b33eb405fb
hbm1812 <111293086+hbm1812@users.noreply.github.com>
2022-09-14 07:55:41 +0700

    Update Sql products

--- assets/sql/products.sql
+++ assets/sql/products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 13, 2022 at 08:57 AM
+-- Generation Time: Sep 14, 2022 at 02:53 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
